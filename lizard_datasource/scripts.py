833bf804c465f8f0bd722e7e4cc295aef75d511f
Reinout van Rees <reinout@vanrees.org>
2014-11-07 14:54:03 +0100

    Look for a couple of days of data instead of grabbing two full months.
    The 60 day period resulted in a 3 month fewsjdbc query, cached for 20 hours...

--- lizard_datasource/scripts.py
+++ lizard_datasource/scripts.py
@@ -79,7 +79,7 @@ def cache_latest_values(ds):
             if cache.timestamp:
                 start_datetime = cache.timestamp
             else:
-                start_datetime = dates.utc_now() - datetime.timedelta(days=60)
+                start_datetime = dates.utc_now() - datetime.timedelta(days=6)
 
             timeseries = drawable.timeseries(
                 location.identifier,
